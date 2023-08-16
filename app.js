800dd2450f7f6fd3ec422ed47cc8a878a79e06a3
DvirBitan123 <dvirbitan123455@gmail.com>
2023-08-16 17:09:25 +0300

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt')
 const app = express()
 const port = 3000
+
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('hello to everyone')
@@ -24,21 +27,84 @@ app.get('/oneUser/:id', (req,res) => {
     res.send(chosenUser);
 })
 
+app.post('/addUser', (req, res) => {
+    let len = usersArray.length;
+    const newId = uuidv4();
+    const newEmail = req.body.email;
+    const userPassword = req.body.password;
+    const saltRounds = 10;
+    bcrypt.hash(userPassword, saltRounds)
+    .then((newPassword) => {
+        for (let i =0; i < len; i++) {
+            if (newId === usersArray[i].id) return ("ERROR: the id is allready exist");
+            const newUser = {
+                id: newId,
+                email: newEmail,
+                password: newPassword
+            };
+            usersArray.push(newUser);
+            res.send(usersArray)
+        }
+    })
+    .catch(err => console.log(err))
+})
+
+app.put('/editUser/:id', (req, res) => {
+    const id = req.params.id;
+    const newId = uuidv4();
+    const newEmail = req.body.email;
+    const newPassword = req.body.password;
+    const saltRounds = 10;
+    bcrypt.hash(newPassword, saltRounds)
+    .then((hash) => {
+        for (i = 0; i < usersArray.length; i++) {
+            if (newId !== usersArray[i].id) {
+                if (id === usersArray[i].id) {
+                    usersArray[i].id = newId;
+                    usersArray[i].email = newEmail;
+                    usersArray[i].password = hash;
+                }
+            } else {
+                res.send("ERROR: the new id is allready exists in the array")
+            }
+        }
+    })
+    res.send(usersArray)
+})
+
 app.delete('/deleteUser/:id', (req,res) => {
     const id = req.params.id;
-    res.send("hattt");
-    let chosenUser = "";
+    let deleteIndex = 0;
     for (let i = 0; i < usersArray.length; i++) {
-        if (id === usersArray[i].id) {
-            delete usersArray[i];
+        if (id === usersArray[i].id) { 
+            deleteIndex = i;  
         }
     }
-    res.send("the user deleted");  //לא גמור
+    usersArray.splice(deleteIndex,1);
+    res.send(usersArray)
+})
+
+app.post('/checkDetails', (req, res) => {
+    const userEmail = req.body.email;
+    const userPassword = req.body.password;
+    let count = 0;
+    for (let i = 0; i < usersArray.length; i++) {
+        if(usersArray[i].email === userEmail) {
+            const hashPassword = usersArray[i].password;
+            bcrypt.compare(userPassword, hashPassword)
+            .then((flag) => {
+                if (flag) count++;
+            })      
+        }  // worked without hash, not working with hash. need to fix it.
+    }
+    if (count > 0) res.send("User is connected");
+    else res.send("Wrong credentials");
 })
 
 app.listen(port, () => {
     console.log(`Server is up and running on port:${port}`);
 })
+
 
 let usersArray = [
     {
@@ -57,3 +123,8 @@ let usersArray = [
         password: "7891011"
     }
 ]
+
+
+
+
+
